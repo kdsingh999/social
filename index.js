eb725e15b21b98b2068b3195ebeef8f1ab50933c
KD Singh <rajputkdsingh999@gmail.com>
2023-04-14 00:41:50 +0530

    create our controller

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const router = require('./routes/index');
 const port = 8000;
 
+app.use('/', router);
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error:${err}`);
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const homeController = require('../controllers/home_controller');
+router.get('/', homeController.home);
+
+module.exports = router;
